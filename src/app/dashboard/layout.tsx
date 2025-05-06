57f74494cccacd46988fa3a7ffc6dd83b9995ab7
Dinesh Nawin <dineshnawin22@gmail.com>
2025-05-06 17:12:36 +0530

    purple color and theme provider added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import { ThemeSelector } from "@/components/theme-selector"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -20,7 +21,7 @@ export default function DashBoardLayout({ children }: { children: ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 justify-between">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
@@ -37,6 +38,9 @@ export default function DashBoardLayout({ children }: { children: ReactNode }) {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
+          </div>
+          <div className="pr-4">
+            <ThemeSelector/>
           </div>
         </header>
         <div className="p-4">{children}</div>
